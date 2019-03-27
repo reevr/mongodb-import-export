4d4c24f5693687fe11058b7e37b59f284cbecad8
reevr <reev.r00@gmail.com>
2019-03-27 19:07:09 +0530

    import and map-group funcationality implemented

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+const path = require('path')
 
 function connect(url) {
   return new Promise((resolve, reject) => {
@@ -13,11 +14,25 @@ function connect(url) {
   })
 }
 
-async function boot() {
-  const connection = await connect('mongodb://localhost:27017')
+async function boot(type, dbName, collection, filePath, url = 'mongodb://localhost:27017') {
+  const connection = await connect(url)
   var mongoUtil = require('./mongo-util')(connection);
 
-  mongoUtil.export('absentia', 'fbtargetings');
+  if (type === 'export') {
+    await mongoUtil.export(dbName, collection);
+  } else if (type == 'import') {
+    await mongoUtil.import(dbName, collection, filePath);
+  } else {
+    await mongoUtil.createOrUpdateGroup(dbName, collection, filePath);
+  }
 }
 
-boot().then(() => console.log('done'));
+const type = process.argv[2] || 'export';
+const dbName = process.argv[3] || 'absentia';
+const collection = process.argv[4] || 'fbtargetings';
+const filePath = process.argv[4] ? path.join(__dirname, './output', process.argv[4]) : path.join(__dirname, './output', 'mongodb-export.xlsx');
+
+boot(type, dbName, collection, filePath).then(() => console.log('done'));
+
+
+console.log('******', type)
--- mongo-util.js
+++ mongo-util.js
@@ -1,18 +1,139 @@
 var Excel = require('exceljs');
 var path = require('path');
 
-function functions(connection) {
+function mongoUtility(connection) {
 
-  function Import(bName, collection) {
-    var workbook = new Excel.Workbook();
-    var worksheet = workbook.addWorksheet('import-data');
+  async function Import(dbName, collection, filePath) {
+    try {
+      var db = setDB(dbName);
+      collection = setCollection(db, collection);
+      var workbook = new Excel.Workbook();
+      await workbook.xlsx.readFile(filePath)
+      var worksheet = workbook.getWorksheet(1);
+      const data = [];
+      let columns;
+      worksheet.eachRow((row, rowNumber) => {
+        if (rowNumber == 1) {
+          const [empty, ...values] = row.values;
+          delete empty;
+          columns = values;
+        }
+        data.push(getKeyValueMapping(columns, row.values))
+      });
+      await insertManyDocuments(data, collection)
 
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  function insertManyDocuments(data, collection) {
+    return new Promise((resolve, reject) => {
+      collection.insertMany(data, function (err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+      });
+    });
+  }
+
+  function getKeyValueMapping(columns, [empty, ...values]) {
+    const obj = {};
+    values.forEach((value, index) => {
+      if (columns[index] !== '_id') {
+        obj[columns[index]] = value;
+      }
+    });
+    return obj;
+  }
+
+  async function createOrUpdateGroup(dbName, collection, filePath) {
+    try {
+      var db = setDB(dbName);
+      var workbook = new Excel.Workbook();
+      await workbook.xlsx.readFile(filePath);
+      const worksheet = workbook.getWorksheet(1);
+
+      const groupTargetingMap = groupByGroupName(worksheet);
+      const _idIndex = worksheet.getRow(1).values.findIndex(col => col == '_id');
+      const results = await Promise.all(syncWithDB(groupTargetingMap, db, _idIndex))
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  function syncWithDB(groupTargetingMap, db, _idIndex) {
+    const collection = setCollection(db, 'groups');
+    return Object.keys(groupTargetingMap).map(groupName => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const result = await findDocument(collection, { group_name: groupName });
+          let targetingIds = groupTargetingMap[groupName].map(value => value[_idIndex])
+          if (!!result) {
+            result.targeting_ids = [...result.targeting_ids, ...targetingIds];
+            await upsertDocument({ group_name: groupName }, { targeting_ids: result.targeting_ids }, collection);
+          } else {
+            await upsertDocument({ group_name: groupName }, { targeting_ids: result.targetingIds }, collection);
+          }
+          resolve(true);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    })
+  }
+
+  function upsertDocument(searchQuery, newValues, collection) {
+    return new Promise((resolve, reject) => {
+      collection.update(
+        searchQuery,
+        newValues,
+        { upsert: true, safe: false },
+        function (err, data) {
+          if (err) {
+            return reject(err);
+          }
+          resolve(data);
+        }
+      );
+    })
+  }
+
+  function findDocument(collection, query) {
+    return new Promise((resolve, reject) => {
+      collection.findOne(query, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      })
+    });
+  }
+
+  function groupByGroupName(worksheet) {
+    let groupIndex;
+    const group = {};
+    worksheet.eachRow(function (row, rowNumber) {
+      if (rowNumber == 1) {
+        groupIndex = row.values.findIndex(column => column == 'group');
+      } else {
+        groupBy(row.values[groupIndex], row.values, group);
+      }
+    });
+    return group;
+  }
+
+  function groupBy(keys, value, output) {
+    keys = keys.split(",").map(val => val.trim());
+    keys.forEach(key => {
+      if (!output[key]) {
+        output[key] = [];
+      }
+      output[key].push(value);
+    })
   }
 
   function Export(dbName, collection) {
     var db = setDB(dbName);
     collection = setCollection(db, collection);
-    getData(collection, {})
+    return getData(collection, {})
       .then(data => createWorksheet(data))
       .then(() => console.info('Exported succesfully'))
       .catch(err => console.error(err));
@@ -50,6 +171,7 @@ function functions(connection) {
           worksheet.addRow(row);
         }
       }
+
       const filename = +(new Date) + ".xlsx"
       const filePath = path.join(__dirname, './output', filename)
       workbook.xlsx.writeFile(filePath)
@@ -80,14 +202,8 @@ function functions(connection) {
 
   return {
     import: Import,
-    export: Export
+    export: Export,
+    createOrUpdateGroup: createOrUpdateGroup
   }
 }
-
-
-
-
-
-
-
-module.exports = functions
+module.exports = mongoUtility
